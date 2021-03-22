41fb8c6e4b6d1213760990f62279e90c29cfa4fb
k4-1 <80098610+k4-1@users.noreply.github.com>
2021-03-22 15:23:37 +0800

    Update lcd_spi.cpp

--- Arduino_LCD-SPI_library/lcd_spi.cpp
+++ Arduino_LCD-SPI_library/lcd_spi.cpp
@@ -12,7 +12,7 @@ lcd_spi::lcd_spi(uint8_t ssPin)
 
 void lcd_spi::initSPI(uint8_t ssPin)
 {
-    cs = ssPin;
+	cs = ssPin;
 	pinMode(cs,OUTPUT);
 	digitalWrite(cs,HIGH);
 	SPI.begin();
@@ -24,8 +24,8 @@ void lcd_spi::begin(uint8_t _cols, uint8_t _rows, uint8_t _charsize)
 	cols = _cols;
 	rows = _rows;
 	charsize = _charsize;
-
-    delayMicroseconds(50000);
+	
+	delayMicroseconds(50000);
 
 	command(FUNCTIONSET | _2LINE);
 	delayMicroseconds(10000);
@@ -59,7 +59,8 @@ void lcd_spi::clear()
 void lcd_spi::setCursor(uint8_t col, uint8_t row)
 {
 	int row_offsets[] = { 0x00, 0x40, 0x14, 0x54 };
-	if (row > rows) {
+	if (row > rows) 
+	{
 		row = rows-1;    // we count rows starting w/0k
 	}
 	command(LCD_SETDDRAMADDR | (col + row_offsets[row]));
@@ -67,7 +68,7 @@ void lcd_spi::setCursor(uint8_t col, uint8_t row)
 
 inline void lcd_spi::command(uint8_t value)
 {
-    int buff[2];
+	int buff[2];
 	buff[0] = 0;
 	buff[0] |= value >> 1;
 	buff[1] = (value & 1) << 7;
@@ -75,32 +76,30 @@ inline void lcd_spi::command(uint8_t value)
 	
 	digitalWrite(cs,LOW);
 	SPI.transfer(buff[0]);
-    SPI.transfer(buff[1]);
+	SPI.transfer(buff[1]);
 	digitalWrite(cs,HIGH);
-    delayMicroseconds(1);
+	delayMicroseconds(1);
 }
 
 inline void lcd_spi::write(uint8_t value)
 {
-    int buffs[2];
+	int buffs[2];
 	buffs[0] = 0x80;
 	buffs[0] |= value >> 1;
 	buffs[1] = (value & 1) << 7;
-	delayMicroseconds(10000); 
-
-    digitalWrite(cs,LOW);
-    SPI.transfer(buffs[0]);
-    SPI.transfer(buffs[1]);
+	delayMicroseconds(10000);
+	digitalWrite(cs,LOW);
+	SPI.transfer(buffs[0]);
+	SPI.transfer(buffs[1]);
 	digitalWrite(cs,HIGH);
-	
-    delayMicroseconds(1);   
+	delayMicroseconds(1);   
 }
 
 void lcd_spi::print(const char c[])
 {
 	for(int i =0; i < strlen(c); i++ ) 
 	{
-      char x = c[i];
-	  write((int)x);
+		char x = c[i];
+		write((int)x);
 	}
 }
