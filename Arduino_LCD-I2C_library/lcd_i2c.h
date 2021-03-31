a5dbecbe5ec489488d4bc15eac348fac51f2abb6
k4-1 <80098610+k4-1@users.noreply.github.com>
2021-03-31 17:22:48 +0800

    Update 1.0
    -two features added; scroll left to right; scroll right to left
    -Modify the print structure

--- Arduino_LCD-I2C_library/lcd_i2c.h
+++ Arduino_LCD-I2C_library/lcd_i2c.h
@@ -4,9 +4,11 @@
 #include <inttypes.h>
 #include <Arduino.h>
 #include <Wire.h>
+#include <Print.h>
 
 //Instruction Set
 #define CLEARDISPLAY 0X01
+#define CURSORSHIFT 0x10
 
 //display entry mode
 #define ENTRYMODESET 0X04
@@ -14,6 +16,12 @@
 #define ENTRYRIGHT 0X00
 #define ENTRYSHIFTINCREMENT 0X01
 #define ENTRYSHIFTDECREMENT 0X00
+
+// flags for display/cursor shift
+#define DISPLAYMOVE 0x08
+#define CURSORMOVE 0x00
+#define MOVERIGHT 0x04
+#define MOVELEFT 0x00
 
 //display control
 #define DISPLAYCONTROL 0X08
@@ -23,8 +31,8 @@
 #define CURSOROFF 0X00
 #define BLINKON 0X01
 #define BLINKOFF 0X00
-#define LCD_SETCGRAMADDR 0x40 
-#define LCD_SETDDRAMADDR 0x80
+#define SETCGRAMADDR 0x40 
+#define SETDDRAMADDR 0x80
 
 //for functionset
 
@@ -36,7 +44,7 @@
 #define _8BITMODE 0x10
 #define _4BITMODE 0x00
 
-class lcd_i2c{
+class lcd_i2c : public Print{
 public:
   lcd_i2c(uint8_t _addr, uint8_t _cols, uint8_t _rows, uint8_t _charsize = _5x8DOTS);
 
@@ -44,13 +52,14 @@ public:
   void clear();
   void noDisplay();
   void display();
+  void scrollDisplayRight();
+  void scrollDisplayLeft();  
   
-  
-  void  setCursor(uint8_t, uint8_t);
-  void write(uint8_t);
+  void setCursor(uint8_t, uint8_t);
+  virtual size_t write(uint8_t);
   void command(uint8_t);
 
-  void print(const char[]);
+  void printstr(const char[]);
   
 private:
 
@@ -61,6 +70,7 @@ private:
   uint8_t row;
   uint8_t charsize;
   uint8_t _command;
+  uint8_t _displaymode;
   int buff;
   int buffs;
   uint8_t _displayfunction;  
